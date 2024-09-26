bf0e0569edc9a65af5605ec95e15b911a75a11b2
ilaydacansev <126075093+ilaydacansev@users.noreply.github.com>
2024-09-26 18:06:26 +0300

    fix: change photo and add javascript codes

---
+++ app.js
@@ -0,0 +1,12 @@
+function toggleDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.remove("show");
+    }
+  }
+};
