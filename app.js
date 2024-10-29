9644ffdce89a076d89877e331e0f14887381a870
İlayda Cansev <icansev@gmail.com>
2024-10-29 16:18:09 +0300

    .

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// function toggleDropdown() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
-
-// window.onclick = function (event) {
-//   if (!event.target.matches(".dropbtn")) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     document.getElementById("searchBar").value = "";
-//     for (var i = 0; i < dropdowns.length; i++) {
-//       dropdowns[i].classList.remove("show");
-//     }
-//   }
-// };
-
-const app = document.getElementById("app");
 const timeElement = document.getElementById("time");
 const dateElement = document.getElementById("date");
 const cityElement = document.getElementById("city");
